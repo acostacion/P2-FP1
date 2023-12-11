0fa6c5bcc5aee9205f573fce9c5e45f9ddacf822
Bienen27 <josetg01@ucm.es>
2023-12-11 20:24:24 +0100

    Retoquillos

--- Program.cs
+++ Program.cs
@@ -5,12 +5,14 @@ using System.Drawing;
 using System;
 using System.Runtime.CompilerServices;
 using System.IO;
+using System.Diagnostics;
 
 namespace P2_FP1
 {
     class Program
     {
-        static Random rnd = new Random(); // Generador de aleatorios para colocar los obstáculos verticales.
+        // Generador de aleatorios para colocar los obstáculos verticales.
+        static Random rnd = new Random(); 
 
         #region Constantes
         const bool DEBUG = true; // Para sacar información de depuración en el Renderizado.
@@ -32,19 +34,22 @@ namespace P2_FP1
 
         static void Main()
         {
-            int[] suelo, techo;
-            int fil, ascenso, frame, puntos;
-            bool colision;
+            int[] suelo, techo; // Array columnas suelo y techo.
+            int fil, ascenso, frame, puntos; // Variables relacionadas con el pájaro.
+            bool colision; // Booleano de colisión.
 
             
             // Preguntar si se quiere cargar un juego guardado o iniciar uno nuevo.
             Console.WriteLine("¿Deseas cargar un juego guardado (c) o iniciar uno nuevo (n)?");
+
+            // Console.ReadKey() detecta la siguiente tecla que se presione.
+            // .KeyChar obtiene la tecla presionada.
+            // Se almacena todo ello en opcion.
             char opcion = Console.ReadKey().KeyChar;
 
             if (opcion == 'c')
             {
-                // El carga lleva valores OUT porque se crean aquí, por eso no hace falta inicializar antes, aparte q
-                // en el enunciado no nos vienen inicializados asiq los dejamos como viene mejor.
+                // El carga lleva valores OUT porque se crean aquí.
                 // documento.txt lo he creado dentro de la carpeta Debug !! muy important
                 CargaJuego(FILE, out suelo, out techo, out fil, out ascenso, out frame, out puntos, out colision);
             }
@@ -57,16 +62,19 @@ namespace P2_FP1
             // Renderizado inicial.
             Render(suelo, techo, fil, frame, puntos, colision);
 
+            // Inicialmente ni está pausado ni se ha dado al exit.
             bool pausar = false, salir = false;
 
-            // Bucle principal.
+            // Bucle principal (si no hay colisión ni se ha dado al exit...).
             while(!colision && !salir)
             {
+                // Si no se ha pausado... 
                 if (!pausar)
                 {
                     // Lectura de input.
                     char c = LeeInput();
 
+                    // Pausa...
                     if (c == 'p')
                     {
                         pausar = true; //esto no lo estamos usando, igual se plantea de otra manera ?
@@ -74,10 +82,12 @@ namespace P2_FP1
                         Console.ReadLine();
                         pausar = false;
                     }
+                    // Exit...
                     else if (c == 'q')
                     {
                         salir = true;
                     }
+                    // El juego está llevándose a cabo...
                     else
                     {
                         // Scroll lateral + movimiento pájaro + colisiones + gestión de puntos.
@@ -90,6 +100,7 @@ namespace P2_FP1
                         // Renderizado.
                         Render(suelo, techo, fil, frame, puntos, colision);
 
+                        // Retardo.
                         Thread.Sleep(DELTA);
 
                         frame++;
@@ -106,8 +117,9 @@ namespace P2_FP1
             }
         }
 
-        static void Inicializa(out int[] suelo, out int[] techo, out int fil, out int ascenso, out int frame, out int puntos, out bool colision) //done
+        static void Inicializa(out int[] suelo, out int[] techo, out int fil, out int ascenso, out int frame, out int puntos, out bool colision) 
         {
+            // Inicialización de los arrays suelo y techo.
             suelo = new int[ANCHO];
             techo = new int[ANCHO];
 
@@ -121,13 +133,20 @@ namespace P2_FP1
                 techo[i]= ALTO - 1;    
             }
 
+            // Establecemos la posición inicial de la fila del pájaro en la mitad de la altura de la pantalla.
             fil = ALTO / 2;
+
+            // Inicializamos la variable ascenso en -1, que indica descenso.
             ascenso = -1;
+
+            // Ponemos los contadores de frames y puntos a 0.
             frame = puntos = 0;
+
+            // Al principio no hay colisión.
             colision = false;
         }
 
-        static void Render(int[] suelo, int[] techo, int fil, int frame, int puntos, bool colision) // revisar
+        static void Render(int[] suelo, int[] techo, int fil, int frame, int puntos, bool colision) 
         {
             // Limpia la consola en cada render.
             Console.Clear();
@@ -147,7 +166,7 @@ namespace P2_FP1
                     Console.ResetColor();
                 }
 
-                // SUELO. Recorremos el array de suelo, desde suelo[ALTO-1] hasta suelo[0] (hacia atrás),
+                // SUELO. Recorremos el array de suelo, desde suelo[ALTO] hasta suelo[0] (hacia atrás),
                 // pintando así cada coordenada (i*2,j), en cada vuelta. 
                 for (int j = Convierte(suelo[i]); j <= ALTO; j++)
                 {
@@ -215,20 +234,23 @@ namespace P2_FP1
             #endregion
         }
 
-        static void Avanza(int[] suelo, int[] techo, int frame) // done
+        static void Avanza(int[] suelo, int[] techo, int frame) 
         {
             // NOTA DE JT: la última posición del array es x.Length - 1, no techo.Length.
 
-            // Desplazamos todos los elementos una posición a la izquierda.
+            // Desplazamos todos los elementos una posición a la izquierda (vamos avanzando en el array).
             for (int i = 0; i < techo.Length - 1; i++)
             {
                 techo[i] = techo[i + 1];
                 suelo[i] = suelo[i + 1];
             }
 
+            // Inicializamos s y t, que son variables que van a indicar la siguiente columna.
             int s, t;
 
             // Comprobamos si debemos generar un nuevo obstáculo.
+            // Si el resto de frame entre SEP_OBS es 0 significa que frame es divisible por SEP_OBS,
+            // y por lo tanto, es el momento de generar un nuevo obstáculo en el juego.
             if (frame % SEP_OBS == 0)
             {
                 // Generamos "s" aleatoriamente dentro del rango.
@@ -243,16 +265,16 @@ namespace P2_FP1
             }
             else
             {
-                // Si no es un frame de obstáculo, asignamos valores predeterminados.
+                // Si no es un frame de obstáculo, asignamos valores predeterminados (sin columna).
                 suelo[ANCHO - 1] = 0;
                 techo[ANCHO - 1] = ALTO - 1;
             }
         }
 
         #region Métodos que controlan el movimiento del pájaro 
-        static void Mueve(char ch, ref int fil, ref int ascenso) // done
+        static void Mueve(char ch, ref int fil, ref int ascenso) 
         {
-            //ascenso pasa por referencia para que los valores modificados salgan del método.
+            // ascenso y fil pasan por referencia para que los valores modificados salgan del método.
 
             // Si ch==’i’...
             if (ch == 'i')
@@ -282,21 +304,20 @@ namespace P2_FP1
             }
         }
 
-        static bool Colision(int[] suelo, int[] techo, int fil) // done
+        static bool Colision(int[] suelo, int[] techo, int fil) 
         { 
+            // Si la posición del suelo en COL_BIRD está por debajo o la posición del techo en COL_BIRD está por encima de fil, return true.
             return suelo[COL_BIRD] >= fil || techo[COL_BIRD] <= fil;
         }
 
-        static void Puntua(int[] suelo, int[] techo, ref int puntos) // done.
+        static void Puntua(int[] suelo, int[] techo, ref int puntos) 
         {
-            // puntos pasa por referencia porque se quiere que su valor salga del método.
-
-            // ESTO DE ABAJO ME LO HA SUGERIDO BING CHAT PORQUE YA NO SABÍA CÓMO HACERLO..
+            // Puntos pasa por referencia porque se quiere que su valor salga del método.
 
             // Asumiendo que el pájaro siempre está en la columna COL_BIRD
             if (suelo[COL_BIRD] != 0 || techo[COL_BIRD] != ALTO - 1)
             {
-                // Si hay un obstáculo en la columna del pájaro, incrementa los puntos
+                // Si hay un obstáculo en la columna del pájaro, incrementa los puntos.
                 puntos++;
             }
         }
@@ -314,7 +335,7 @@ namespace P2_FP1
             salida.WriteLine(frame);
             salida.WriteLine(puntos);
 
-            // Buscamos donde hay obstaculo. BUSQUEDA -> WHILE (importante)
+            // Buscamos donde hay obstáculo. BUSQUEDA -> WHILE (importante).
             int i = 0, posicion = 0;
             bool encontrado = false;
 
@@ -330,7 +351,7 @@ namespace P2_FP1
             }
 
             // Escribimos los valores desde posicion, sumando SEP_OBS, hasta llegar a ANCHO.
-            for(int j = posicion; j < ANCHO; j += SEP_OBS)
+            for(int j = posicion; j < ANCHO; j = j + SEP_OBS)
             {
                 salida.Write(suelo[j] + " " + techo[j] + " ");
             }
@@ -341,19 +362,23 @@ namespace P2_FP1
 
         static void CargaJuego(string file, out int[] suelo, out int[] techo, out int fil, out int ascenso, out int frame, out int puntos, out bool colision)
         {
-
+            // Verificar si el archivo existe.
             if (File.Exists(file))
             {
+                // Declaración de flujo de entrada.
                 StreamReader entrada = new StreamReader(file);
 
+                // Leer las variables.
                 fil = int.Parse(entrada.ReadLine());
                 ascenso = int.Parse(entrada.ReadLine());
                 frame = int.Parse(entrada.ReadLine());
                 puntos = int.Parse(entrada.ReadLine());
 
+                // Inicializar los arrays suelo y techo.
                 suelo = new int[ANCHO];
                 techo = new int[ANCHO];
 
+                // Establece de 0 a ANCHO - 1 las posiciones iniciales de las columnas.
                 for (int i = 0; i < ANCHO; i++)
                 {
                     suelo[i] = 0;
@@ -361,27 +386,41 @@ namespace P2_FP1
                 }
 
                 // Leer los obstáculos guardados en el archivo y actualizar los arrays suelo y techo
-                int posicion = int.Parse(entrada.ReadLine());
+                int posicion = int.Parse(entrada.ReadLine()); // Posición inicial.
                 string linea;
 
+                // Mientras haya línea que leer...
                 while ((linea = entrada.ReadLine()) != null)
-                { 
-                    // Creamos un array de los elementos separados por " "
-                    string[] valores = linea.Split(" ", StringSplitOptions.RemoveEmptyEntries); 
+                {
+                    // 1. Declara un array de strings llamado valores.
+                    // 2. Split divide la cadena linea en subcadenas cada vez que encuentra un (" ").
+                    // 3. StringSplitOptions.RemoveEmptyEntries es para indicar que se eliminen las subcadenas vacías del resultado.
+                    string[] valores = linea.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-                    for (int i = 0; i < valores.Length; i += 2) // En vez de i++, vamos sumando i=i+2.
+                    // En vez de i++, vamos sumando i=i+2. Va iterando de par en par.
+                    for (int i = 0; i < valores.Length; i += 2) 
                     {
+                        // Almacenar el valor en la posición i del valor del suelo.
                         string valorSuelo = valores[i];
+
+                        // Almacenar el valor en la posición i del valor del techo.
                         string valorTecho = valores[i + 1];
 
+                        // Hacer que valorSuelo se almacene en la posición actual del array.
                         suelo[posicion] = int.Parse(valorSuelo);
+
+                        // Hacer que valorTecho se almacene en la posición actual del array.
                         techo[posicion] = int.Parse(valorTecho);
 
+                        // Hacer que la posición vaya cada SEP_OBS.
                         posicion += SEP_OBS;
                     }
                 }
 
+                // Cierre de flujo.
                 entrada.Close();
+
+                // Carga sin haber colisionado.
                 colision = false;
             }
             else
